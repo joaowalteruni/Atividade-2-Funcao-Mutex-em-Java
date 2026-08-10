fb5033f73aaa6f262235827f449aa4cdb962c0c4
João Walter Moraes de Almeida <joaowalmeida@edu.unifil.br>
2026-08-10 20:13:47 -0300

    Aplicada Função Mutex em Java
    Implement a multi-threaded process simulation with critical section handling.

---
+++ ProgramaWin64.java
@@ -0,0 +1,44 @@
+public class ProgramaWin64 { 
+
+static int travado = 0;
+
+public static void rodarProcesso(String nome ) {
+  System.out.println("[" + nome  + "] tentando entrar ...");
+
+  while (travado == 1 ) {
+    System.out.println("[" + nome + "] recurso ocupado !aguardando ...");
+    try{
+      Thread.sleep(1000);
+    }catch (Exception e ) {
+
+    } 
+  }
+  
+  travado = 1;
+  System.out.println("--> [" + nome + "] Entrou na seção critica!");
+  try{
+    Thread.slepp(4000);
+  }catch(Exception e ) {
+
+  }
+
+  travado = 0;
+  System.out.println("<-- [" + nome + "] saiu da seção critica");
+
+  public static void main(String[] args)
+
+    read p1 = new Thread(new Runnable() { 
+   public void run() { 
+   rodar processo("Processo_A);
+   }
+    ;
+
+ read p2 = newThread(new Runnable() {
+   public void run() { 
+     rodarProcesso(Processo_B);
+   }
+   ;
+
+   .start();
+   .start();
+                                             
